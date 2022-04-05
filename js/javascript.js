b8cdb62113ee58a22c09aea4d77b114722ce3042
joshdixon0515 <43324427+joshdixon0515@users.noreply.github.com>
2022-04-04 17:55:38 -0700

    added tab navigation setup

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
-    console.log("ready!");
+    tabDisplay = document.getElementsByClassName("tabcontent");
+    for(i=0; i<tabDisplay.length; i++){
+        tabDisplay[i].style.display = "none";
+    }
 });
 function foo(){
     var audio = new Audio('audio//crazy_pills.mp3');
     audio.play();
+}
+
+function openTab(evt, tabName){
+    var i;
+    var tabDisplay;
+    var tabLinks;
+    tabDisplay = document.getElementsByClassName("tabcontent");
+    for(i=0; i<tabDisplay.length; i++){
+        tabDisplay[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName("tablinks");
+    for(i=0; i<tabLinks.length; i++){
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
